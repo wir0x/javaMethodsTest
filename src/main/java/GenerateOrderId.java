a7c70856414a563f6b4b64e117f0ec9c7bdc0f99
gonzalosv <gonzalo.salazar@swissbytes.ch>
2015-12-03 18:17:07 -0400

    fix minor bug

--- src/main/java/GenerateOrderId.java
+++ src/main/java/GenerateOrderId.java
@@ -6,9 +6,9 @@ public class GenerateOrderId {
         String key2 = getNameKey(name2, 8);
         String key3 = getRandomId();
         String orderId = key1 + key2 + key3;
-        System.out.println(orderId);
         return orderId;
     }
+
 
     public String getNameKey(String name, int length) {
         String key = "";
@@ -21,17 +21,50 @@ public class GenerateOrderId {
         return key;
     }
 
-    public int getLengthStr(String name, int length) {
-        int LENGTH = length;
-        return name.length() < LENGTH ? name.length() : LENGTH;
+    public static String orderIdToWeb(String id1, String id2, String id3, String id4) {
+        String key1 = setZero(id1, 6);
+        String key2 = setZero(id2, 6);
+        String key3 = setZero(id3, 2);
+        String key4 = setZero(id3, 6);
+        String orderId = key1 + key2 + key3 + key4;
+        return orderId.length() > 20 ? getDefaultKey(orderId, 20) : orderId;
     }
 
-    public String getRandomId() {
-        int _iKey = new Random().nextInt(999);
+    public static String getDefaultKey(String name, int length) {
+        String key = "";
+        String _name = removeSpace(name);
+
+        int lengthName = getLengthStr(name, length);
+        for (int i = 0; i < lengthName; ++i)
+            key += _name.substring(i, i + 1);
+        return key;
+    }
+
+    private static int getLengthStr(String str, int length) {
+        return str.length() < length ? str.length() : length;
+    }
+
+    private static String getRandomId() {
+        int _iKey = new Random().nextInt(9999);
         return String.valueOf(_iKey);
     }
 
-    public String removeSpace(String str) {
+    public static String setZero(String number, int length) {
+        String key = "";
+        String _number = removeSpace(number);
+        int _length = getLength(_number, length);
+        if (_number.length() > length) return getDefaultKey(_number, length);
+        for (int i = _length; i < length; ++i) key += "0";
+        key += _number;
+        return key;
+    }
+
+    private static String removeSpace(String str) {
         return str.replaceAll("\\s+", "");
     }
+
+    private static int getLength(String str, int length) {
+        return str.length() > length ? length : str.length();
+    }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,16 @@ public class Main {
 //        System.out.println(validateInteger.isNumeric("77355967 "));
 
         GenerateOrderId generateOrderId = new GenerateOrderId();
-        String name1 = "DAGA";
-        String name2 = "daga6765";
-        generateOrderId.printOrderId(name1, name2);
+        String name1 = "1234567891234567891234567894564654654";
+        String name2 = "656";
+        String name3 = "45";
+        String name4 = "123";
+
+//        System.out.println(GenerateOrderId.orderIdToWeb(name1, name2, name3, name4));
+//        System.out.println(generateOrderId.orderIdToWeb(name1, name2, name3, name4).length());
+        System.out.println(GenerateOrderId.setZero(name1, 20));
+        System.out.println(GenerateOrderId.setZero(name1, 20).length());
+        System.out.println(GenerateOrderId.getDefaultKey(name1, 10));
+//        generateOrderId.printOrderId(name1, name2);
     }
 }
